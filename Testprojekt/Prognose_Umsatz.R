9f67a85575e2cb4481a16feb814431963af36d90
Sascha Geppert <email@saschageppert.de>
2014-12-18 19:22:46 +0100

    Fixes

--- Testprojekt/Prognose_Umsatz.R
+++ Testprojekt/Prognose_Umsatz.R
@@ -4,17 +4,12 @@ trendwerte = c()
 prognoseUmsatz = c()
 umsatzaggregiert = c()
 
-confidenceLevel = 0.90
+# default values
+confidenceLevel = 0.95
 
 
-
-
-
-# TODO: Währungsart beachten
-
-# Fragen: Warum umsatzaggregiert KW 21 - 24 == 0.0?
-
-# SASCHA
+# TODO: Support currency 
+# TODO: Append weeknumbers without any sales
 
 # import data -> data frame
 sales.csv = read.table(file = "./data/AA_Umsatzbelege.csv", header = TRUE, sep=";")
@@ -27,7 +22,7 @@ sales.aggregated <- aggregate(
   sales.csv$BETRAG, 
   list(
     companycode=sales.csv$BUCHUNGSKREIS,
-    weeknumber=as.numeric(format(as.Date(sales.csv$AUSGLEICHDATUM, format = "%d.%m.%Y"), "%U"))
+    weeknumber=as.numeric(format(as.Date(sales.csv$AUSGLEICHDATUM, format = "%d.%m.%Y"), "%U")) + 1
   ), 
   function(x){
     # TODO: maybe merge into sum function
@@ -35,46 +30,39 @@ sales.aggregated <- aggregate(
   }
 )
 
-# TODO: merge this into upper function
-# Supports multiple company codes
-sales.prediction <- by(sales.aggregated, list(companycode=sales.aggregated$companycode), function(x) {
+# Support for multiple company codes
+sales.prediction.list <- by(sales.aggregated, list(companycode=sales.aggregated$companycode), function(x) {
   
   # removes companycode from data.frame
-  y <- subset(x, select= -companycode)
+  # y <- subset(x, select= -companycode)
 
   sales.wn.max <- max(x$weeknumber)
   sales.aggregated.length <- nrow(sales.aggregated)
   
   # linear regression
-  sales.aggregated.lm <- lm(x ~ weeknumber, data = y)
+  sales.aggregated.lm <- lm(x ~ weeknumber, data = x)
   
   # newdata with variables with which to predict
   sales.prediction.data <- data.frame(weeknumber=seq(sales.wn.max + 1, sales.wn.max + 1 + round(sales.aggregated.length/3), 1))
-  
+
   # prediction
-  # --> Serkan
-  return(predict(sales.aggregated.lm, sales.prediction.data,  se.fit = TRUE, interval = "confidence", level = confidenceLevel))
+  sales.prediction.predict <- predict(sales.aggregated.lm, sales.prediction.data,  se.fit = TRUE, interval = "confidence", level = confidenceLevel)
+  
+  # returns df of weeknumber, prediction, and confidence level
+  sales.prediction.frame <- data.frame(sales.prediction.data$weeknumber, sales.prediction.predict, confidenceLevel)
+  names(sales.prediction.frame) <- c("weeknumber", "fit", "min", 'max', 'sefit', 'df', 'residualscale','confidence')
+  
+  return (sales.prediction.frame)
 })
 
+# implode list of dataframes to dataframe
+sales.prediction.df = do.call(rbind.data.frame, sales.prediction.list)
 
-data.frame(sales.prediction$AA)
-
-
-# clean up
-#rm(list = ls())
-
-# DEL
-
-# split imported data.frame using the weeknumber of AUSGLEICHSDATUM to new list
-# sales.csv.weeknumber <- split(sales.csv, format(as.Date(sales.csv$AUSGLEICHDATUM, format = "%d.%m.%Y"), "%U"));
-
-# sum values where SOLL.HABEN.KENNZSICHEN equals "S"
-# sales.aggregated <- lapply(sales.csv.weeknumber, function(x) {
-#  return(sum(x$BETRAG[which(x$SOLL.HABEN.KENNZSICHEN=="S")]))
-#})
+sales.prediction.df
 
 # clean up
-# rm(list = ls())
+rm(list = ls())
+
 
 
 # TMP
